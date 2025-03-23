89096aa6c336e140b2baaef9cb32f8a18422fb4f
unknown <rosa.angel.daniel@gmail.com>
2025-03-22 18:48:46 -0600

    completed HTML and added task option in js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
-const helloWorld = () => {
-    console.log('Hello World');
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    const taskBtn = document.getElementById("taskBtn");
+    const urgentCheck = document.getElementById("urgentCheck");
+    const taskClear = document.getElementById("clearTskBtn");
 
-helloWorld();
+    taskBtn.addEventListener("click", () => {
+        const taskText = taskInput.value.trim();
+        if (taskText === "") {
+            alert("Please enter a task");
+            return;
+        }
+
+        const taskItem = document.createElement("li");
+        taskItem.innerText = taskText;
+        taskList.appendChild(taskItem);
+    });
+});
+
+
