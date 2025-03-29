f9cd6a5e9be0db851114680606857c8595b28249
unknown <rosa.angel.daniel@gmail.com>
2025-03-28 18:14:09 -0600

    completed readme file

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# task-manager
+# Task Manager
+Site link: https://b3hold23.github.io/task-manager/
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [Tests](#tests)
+## Description
+This README is more for practice on how to make read me files for projects. With that being said let me tell you about this project. This is a Task Manager project, When you want to add a task you can add it into the input and either press the enter key or press the add button. If you need to make it urgent You can click the urgent button on the side of the task and it will change the color of the task to red. Once the task is completed you can click on it and it will change the color of the task to green. You can double click the task to delete the specific task.  
+
+## Features
+* Add Tasks: Users can add task using the input field and the "Add Task" button.
+
+* Mark Tasks as Urgent: Each task has an "Urgent" button that changes the task's color to indicate the urgency (changes the color to red).
+
+* Mark Tasks as complete: Clicking a task marks it as completed by changing its color to green.
+
+* Remove Task: Double- clicking on a task will delete it from the list and delete it from local storage.
+
+* Clear All Button: By clicking the "Clear All" Button it will remove all of the tasks from the list and will delete them from the local storage.
+
+* Persistent storage: The task that you create get saved to the Local storage and are retrieved when the page is reloaded.  
+
+## Installation
+If you would like to download this code, you can download the ZIP file from the github repository. 
+
+## Usage
+Ok, his part will have information on the code that I used to make this project work. With that being said let us begin. This project mainly uses JavaScript to create Elements that will contain the task text. This project is used for a place to store your task and let you mark some of the task from urgent and completed tasks as well. I am using the createElement() method to create the list of tasks and their urgent buttons. I use addEventListener() method to be able to add functionality to the site when you click on the tasks, add or delete the tasks. Tasks are saved using localStorage.setItem() and retrieved with localStorage.getItem() to ensure persistence across page reloads.
+## Contribute
+If you’d like to contribute, feel free to suggest improvements to the README or provide feedback on the code. Contact details are below.
+## Tests
+I will post the test of the app soon.
+
+## My Info
+My github:
+<br>
+b3hold23 https://github.com/b3hold23 
+<br>
+My email:
+<br>
+ rosa.angel.daniel@gmail.com
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-    const taskBtn = document.getElementById("taskBtn");
-    const taskClear = document.getElementById("clearTskBtn");
+document.addEventListener("DOMContentLoaded", () => {//This ensures that the script runs after the page has loaded
+    const taskInput = document.getElementById("taskInput");//This gets the taskInput element from the HTML to be manipulated by the script
+    const taskList = document.getElementById("taskList");//This gets the taskList element from the HTML to be manipulated by the script
+    const taskBtn = document.getElementById("taskBtn");//This gets the taskBtn element from the HTML to be manipulated by the script
+    const taskClear = document.getElementById("clearTskBtn");//This gets the clearTskBtn element from the HTML to be manipulated by the script
     
     
     //When the Add Task button is clicked, the task text is added to the list and saved to local storage
-    taskBtn.addEventListener("click", () => {
-        const taskText = taskInput.value.trim();
-        if (taskText === "") {
-            alert("Please enter a task");
-            return;
+    taskBtn.addEventListener("click", () => { //This listens for a click on the Add Task button, then it will run the function
+        const taskText = taskInput.value.trim(); //This gets the value of the taskInput element and removes any white space
+        if (taskText === "") { //If the task text is empty, an alert will pop up
+            alert("Please enter a task"); //This is the alert that pops up if the task text is empty
+            return; //This stops the function from running if the task text is empty
         }
         
         addTask(taskText); //Runs the addTask function
@@ -21,31 +21,30 @@ document.addEventListener("DOMContentLoaded", () => {
     
     
     //When the Enter key is pressed, the task text is added to the list and saved to local storage
-    taskInput.addEventListener('keypress', (e) => {
-        if (e.key === "Enter") {
-            taskBtn.click();
-            console.log("Enter key pressed");
+    taskInput.addEventListener('keypress', (e) => { //This listens for a key press in the taskInput element, then it will run the function
+        if (e.key === "Enter") { //If the key pressed is the Enter key, the task text will be added to the list and saved to local storage
+            taskBtn.click(); //This will run the taskBtn click on line 9 when the Enter key is pressed
         }
     });
     
     
-    const addTask = (taskText) => {
-      const taskWrap = document.createElement("div");
-      taskWrap.classList.add('container','text-center' );
+    const addTask = (taskText) => { //This function adds the task text to the list
+      const taskWrap = document.createElement("div"); //This creates a div element
+      taskWrap.classList.add('container','text-center' ); //This adds the class name container and text-center to the div element
 
-      const taskItem = document.createElement('button');
-      taskItem.classList.add('col-md-8', 'btn', 'btn-info', 'mb-2', 'mx-3');
-      taskItem.innerText = taskText;
+      const taskItem = document.createElement('button'); //This creates a button element
+      taskItem.classList.add('col-md-8', 'btn', 'btn-info', 'mb-2', 'mx-3'); //This adds the class name col-md-8, btn, btn-info, mb-2, and mx-3 to the button element
+      taskItem.innerText = taskText; //This adds the task text to the button element
 
-      const urgentTask = document.createElement('button');
-      urgentTask.classList.add("col-md-2", 'btn', 'btn-outline-warning', 'mb-2', 'mx-3');
-      urgentTask.innerText = "Urgent";
+      const urgentTask = document.createElement('button'); //This creates a button element
+      urgentTask.classList.add("col-md-2", 'btn', 'btn-outline-warning', 'mb-2', 'mx-3'); //This adds the class name col-md-2, btn, btn-outline-warning, mb-2, and mx-3 to the button element
+      urgentTask.innerText = "Urgent"; //This adds the text "Urgent" to the button element
 
-      urgentTask.addEventListener("click", () => {
-        taskItem.classList.toggle("btn-danger");
-        taskItem.classList.toggle("btn-info");
-        urgentTask.classList.toggle("btn-outline-warning");
-        urgentTask.classList.toggle("btn-warning");
+      urgentTask.addEventListener("click", () => { //This AddEventListener listens for a click on the Urgent button, then it will run the function inside the curly braces
+        taskItem.classList.toggle("btn-danger"); //This toggles the class name btn-danger on the taskItem element
+        taskItem.classList.toggle("btn-info"); //This toggles the class name btn-info on the taskItem element
+        urgentTask.classList.toggle("btn-outline-warning"); //This toggles the class name btn-outline-warning on the urgentTask element
+        urgentTask.classList.toggle("btn-warning"); //This toggles the class name btn-warning on the urgentTask element
       });
 
       taskWrap.appendChild(taskItem);
